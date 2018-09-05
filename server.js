53ff4473864a411af5884b473e8ac3abd5e957dc
ozymandias <onuratasaritas@gmail.com>
2018-09-05 23:34:03 +0300

    Added project page

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs', {
-
-  });
-})
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs', {
+//
+//   });
+// })
 
 app.use(express.static(__dirname + '/public'));
 
@@ -46,6 +46,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req,res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    welcomeMessage: 'Projects, our projects are the best'
   });
 });
 
